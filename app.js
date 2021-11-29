5213b3f409c1d416bb35c2750a6b739428dc259e
bob-justbob <levan.qiqodze-nasidze14@geolab.edu.ge>
2021-11-29 18:37:39 +0400

    did some homeworkd

---
+++ app.js
@@ -0,0 +1,18 @@
+// const numbers = [ 4, 5, 11, 2, 9, 99, 1092, 1]
+
+// numbers.sort(function(a, b) {
+//     return a-b
+// })
+
+// let restal = numbers.filter(numbers => numbers % 2 === 0)
+
+
+// function myFUnc (a, b){
+//     return a + b
+// }
+
+// console.log(restal)
+// console.log(restal.reduce(myFUnc))
+
+
+ 
